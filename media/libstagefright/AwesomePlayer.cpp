f0559e0904ae7534a030c21d540dd1d2591717f3
Chris Elliott <chriselliott@google.com>
2015-12-02 13:22:51 -0800

    DO NOT MERGE Revert "AwesomePlayer: Stop posting buffering events once at EOS"
    This reverts commit 701cac1716d5ddb867a5444ea152d26741f3b397.

--- media/libstagefright/AwesomePlayer.cpp
+++ media/libstagefright/AwesomePlayer.cpp
@@ -845,8 +845,7 @@ void AwesomePlayer::onBufferingUpdate() {
         }
     }
 
-    if ( ((mFlags & PLAYING) && !eos) ||
-         (mFlags & (PREPARING | CACHE_UNDERRUN)) ) {
+    if (mFlags & (PLAYING | PREPARING | CACHE_UNDERRUN)) {
         postBufferingEvent_l();
     }
 }
